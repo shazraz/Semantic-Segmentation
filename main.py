a9c1af1663aa775d863a6ba52b6faee304547288
Shahzad Raza <raza.shahzad@gmail.com>
2018-03-14 21:35:29 +0400

    added scaling and args

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os.path
 import tensorflow as tf
 import helper
 import warnings
+import argparse
 from distutils.version import LooseVersion
 import project_tests as tests
 
@@ -64,14 +65,16 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
     l4_part1 = tf.layers.conv2d_transpose(l7_1x1, num_classes, 4, 2, padding='same', kernel_regularizer=reg)
     # Apply 1x1 convolution to layer 4 output to produce layer 4 class predictions
     l4_part2 = tf.layers.conv2d(vgg_layer4_out, num_classes, 1, (1,1), padding='same', kernel_regularizer=reg)
+    l4_part2_scaled = tf.multiply(l4_part2, 0.01, name='pool4_scaled')
     # Apply skip connections to layer 4
-    l4_merged = tf.add(l4_part1, l4_part2)
+    l4_merged = tf.add(l4_part1, l4_part2_scaled)
     # Upsample the merged layer 4
     l3_part1 = tf.layers.conv2d_transpose(l4_merged, num_classes, 4, 2, padding='same', kernel_regularizer=reg)
     # Apply 1x1 convolution to layer 3 output to produce layer 3 class predictions
     l3_part2 = tf.layers.conv2d(vgg_layer3_out, num_classes, 1, (1, 1), padding='same', kernel_regularizer=reg)
+    l3_part2_scaled = tf.multiply(l3_part2, 0.0001, name='pool3_scaled')
     # Apply skip connection to layer 3
-    l3_merged = tf.add(l3_part1, l3_part2)
+    l3_merged = tf.add(l3_part1, l3_part2_scaled)
     # Upsample the merged layer 3
     output = tf.layers.conv2d_transpose(l3_merged, num_classes, 16, 8, padding='same', kernel_regularizer=reg)
 
@@ -129,7 +132,7 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_l
             loss = sess.run([train_op, cross_entropy_loss],
                             feed_dict={input_image: image, correct_label: mask,
                                        learning_rate: lrate, keep_prob: kp})
-        print("Training loss: ", loss)
+        print("Training loss: ", loss[1])
 
 
 tests.test_train_nn(train_nn)
@@ -137,14 +140,14 @@ tests.test_train_nn(train_nn)
 
 def run():
     # Define data paths
-    data_dir = os.path.join('d:\\')
+    data_dir = os.path.join(args.data_path)
     runs_dir = os.path.join('.', 'runs')
 
     # Define constants
     num_classes = 2
     image_shape = (160, 576)
-    batch_size = 1
-    epochs = 10
+    batch_size = args.batch_size
+    epochs = args.epochs
 
     # Declare tf variables
     learning_rate = tf.placeholder(tf.float32)
@@ -169,8 +172,32 @@ def run():
         train_nn(sess, epochs, batch_size, get_batches_fn, train_op, loss, input_image, gt_label, keep_prob,
                  learning_rate)
         # Save inference samples
-        # helper.save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, input_image)
+        helper.save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, input_image)
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Semantic Segmentation using VGG16-FCN8')
+    parser.add_argument(
+        '-d',
+        '--data_path',
+        type=str,
+        default='d:\\',
+        help='Path to training data'
+    )
+    parser.add_argument(
+        '-e',
+        '--epochs',
+        type=int,
+        default=10,
+        help='# of epochs to train the model'
+    )
+    parser.add_argument(
+        '-b',
+        '--batch_size',
+        type=int,
+        default=2,
+        help='# of samples in batch size'
+    )
+
+    args = parser.parse_args()
     run()
